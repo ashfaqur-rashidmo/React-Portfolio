d1aa3420b238a08fee3a1f989b6ddf7f745df9e0
ashfaqur-rashidmo <ashfaqurrashid1@gmail.com>
2025-01-23 10:25:54 +0600

    first commit

---
+++ src/Components/SkillCard/SkillCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Avatar } from '@mantine/core';
+
+const SkillBadge = ({ skills }) => {
+    return skills.map((skill, index) => (
+        <div key={index} className="flex gap-1 border border-textColor rounded-2xl items-center py-4 px-4 mb-1">
+            <Avatar className='w-[48px] ' variant="transparent" radius="xs" src={`/src/assets/Images/Icons/${skill}.png`} />
+            <div className='text-textColor text-lg'>{skill}</div>
+        </div>
+    ));
+};
+
+const SkillCard = ({ title, skills }) => {
+    return (
+        <div 
+            data-aos="fade-left" 
+            data-aos-duration="500" 
+            className="w-[49%] xl-mx:w-[48%] xs-mx:w-[240%] xxs-mx:w-60 sm-mx:w-[80%] sm-mx:flex-col shadow-[0_0_10px_0_#64FFDA50] rounded-2xl border-2 border-primaryColor p-5 flex flex-col justify-between"
+        >
+            <div className="text-3xl text-[#ffffff] text-center font-bold mb-4">
+                {title}
+            </div>
+            <div className="flex gap-5 justify-center flex-wrap flex-grow items-center">
+                <SkillBadge skills={skills} />
+            </div>
+        </div>
+    );
+};
+
+export default SkillCard;
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SkillCard from './SkillCard';
+import SkillCard from '../SkillCard/SkillCard'
 import { SkillInfo } from '../Projects/CardInfo';
 
 const Skills = () => {
